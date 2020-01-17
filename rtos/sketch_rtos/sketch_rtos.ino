632b0f428a11eebc4eeb3836bd6828bffb69b353
Jianping Huang <jhuang@pivotalpayments.com>
2020-01-17 17:16:52 -0500

    Add MQTT supported

--- rtos/sketch_rtos/sketch_rtos.ino
+++ rtos/sketch_rtos/sketch_rtos.ino
@@ -1,16 +1,27 @@
+#include <ArduinoJson.h>
+
+
+
+
 #if CONFIG_FREERTOS_UNICORE
 #define ARDUINO_RUNNING_CORE 0
 #else
 #define ARDUINO_RUNNING_CORE 1
 #endif
 
-#ifndef LED_BUILTIN
-#define LED_BUILTIN 13
-#endif
+
 
 // define two tasks for Blink & AnalogRead
-void TaskBlink( void *pvParameters );
-void TaskAnalogReadA3( void *pvParameters );
+void TaskMQTT( void *pvParameters );
+void TaskCtrl( void *pvParameters );
+
+
+//Create MQTT Pub message 
+
+/*
+JSON format : { curTemp:22.5,status:heat/cool,destTemp:23.5,unit:celsius/fahrenheit}
+*/
+
 
 // the setup function runs once when you press reset or power the board
 void setup() {
@@ -22,8 +33,8 @@ void setup() {
   Serial.println(ARDUINO_RUNNING_CORE);
   // Now set up two tasks to run independently.
   xTaskCreatePinnedToCore(
-    TaskBlink
-    ,  "TaskBlink"   // A name just for humans
+    TaskMQTT
+    ,  "TaskMQTT"   // A name just for humans
     ,  1024  // This stack size can be checked & adjusted by reading the Stack Highwater
     ,  NULL
     ,  2  // Priority, with 3 (configMAX_PRIORITIES - 1) being the highest, and 0 being the lowest.
@@ -31,7 +42,7 @@ void setup() {
     ,  ARDUINO_RUNNING_CORE);
 
   xTaskCreatePinnedToCore(
-    TaskAnalogReadA3
+    TaskCtrl
     ,  "AnalogReadA3"
     ,  1024  // Stack size
     ,  NULL
@@ -51,48 +62,32 @@ void loop()
 /*---------------------- Tasks ---------------------*/
 /*--------------------------------------------------*/
 
-void TaskBlink(void *pvParameters)  // This is a task.
+void TaskMQTT(void *pvParameters)  // This is a task.
 {
   (void) pvParameters;
 
-/*
-  Blink
-  Turns on an LED on for one second, then off for one second, repeatedly.
-    
-  If you want to know what pin the on-board LED is connected to on your ESP32 model, check
-  the Technical Specs of your board.
-*/
 
-  // initialize digital LED_BUILTIN on pin 13 as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
+  // Sending the status to MQTT Broker
 
   for (;;) // A Task shall never return or exit.
   {
-    digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
     vTaskDelay(1000);  // one tick delay (15ms) in between reads for stability
-    digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-    vTaskDelay(100);  // one tick delay (15ms) in between reads for stability
   }
 }
 
-void TaskAnalogReadA3(void *pvParameters)  // This is a task.
+void TaskCtrl(void *pvParameters)  // This is a task.
 {
   (void) pvParameters;
   
 /*
-  AnalogReadSerial
-  Reads an analog input on pin A3, prints the result to the serial monitor.
-  Graphical representation is available using serial plotter (Tools > Serial Plotter menu)
-  Attach the center pin of a potentiometer to pin A3, and the outside pins to +5V and ground.
-  This example code is in the public domain.
+  Receive the package from MQTT broker and Parse it
+  the data is packed by JSON format
+  
 */
 
   for (;;)
   {
-    // read the input on analog pin A3:
-    int sensorValueA3 = analogRead(A3);
-    // print out the value you read:
     Serial.println(sensorValueA3);
-    vTaskDelay(10000);  // one tick delay (15ms) in between reads for stability
+    vTaskDelay(1000);  // one tick delay (15ms) in between reads for stability
   }
 }
